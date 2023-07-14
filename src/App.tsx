86326a15544a133c9c4eee7b87c87504b54332a9
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-15 00:03:27 +0300

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Hello from vite</h1>
+      <p
+        style={{
+          color: 'red',
+        }}
+      ></p>
+    </>
+  );
+}
+
+export default App;
