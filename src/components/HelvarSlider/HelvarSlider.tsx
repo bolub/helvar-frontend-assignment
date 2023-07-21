9eb5de437599b884344bc9da7ee88e53a8a7eba4
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-21 16:14:27 +0300

    Handle keyboard navigation

--- src/components/HelvarSlider/HelvarSlider.tsx
+++ src/components/HelvarSlider/HelvarSlider.tsx
@@ -1,3 +1,4 @@
+import { FC, useEffect, useRef } from 'react';
 import {
   Flex,
   Text,
@@ -6,34 +7,31 @@ import {
   SliderFilledTrack,
   SliderThumb,
 } from '@chakra-ui/react';
-import { FC } from 'react';
+
+interface CustomSliderProps {
+  defaultValue: number;
+  label: string;
+  onChange: (value: number) => void;
+  testId: string;
+  value: number;
+}
 
 interface LabelProps {
   title: string;
   value: number;
 }
 
-interface CustomSliderProps {
-  label: string;
-  value: number;
-  defaultValue?: number;
-  testId: string;
-  onChange: (value: number) => void;
-}
-
 const Label: FC<LabelProps> = ({ title, value }) => {
   return (
-    <>
-      <Flex justifyContent='space-between' w='100%' alignItems='center'>
-        <Text fontWeight='medium' fontSize='lg'>
-          {title}
-        </Text>
+    <Flex justifyContent='space-between' w='100%' alignItems='center'>
+      <Text fontWeight='medium' fontSize='lg'>
+        {title}
+      </Text>
 
-        <Text fontWeight='extrabold' color='red.500' fontSize='lg'>
-          {value}%
-        </Text>
-      </Flex>
-    </>
+      <Text fontWeight='extrabold' color='red.500' fontSize='lg'>
+        {value}%
+      </Text>
+    </Flex>
   );
 };
 
@@ -44,16 +42,61 @@ const CustomSlider: FC<CustomSliderProps> = ({
   onChange,
   testId,
 }) => {
-  // TODO: Write a unit test for this function
   const getNearestStepValue = (value: number) => {
     if (value <= 1) return value;
-    if (value === 2) return 1;
-    if (value >= 100) return 100;
+    if (value === 2) return 5;
 
-    const nearestStep = Math.round(value / 5) * 5;
-
-    return nearestStep;
+    return Math.round(value / 5) * 5;
   };
+
+  const sliderRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const getIncrementalValue = (value: number) => {
+      const isDraggingUp = value > defaultValue;
+      const isDraggingDown = value < defaultValue;
+
+      if (isDraggingUp) {
+        if (value <= 1) return 1;
+        if (value === 2) return 3;
+      }
+
+      if (isDraggingDown) {
+        if (value <= 1) return 1;
+        if (value === 5) return 4;
+      }
+
+      return 5;
+    };
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (value === 0) return;
+
+      const incrementValue = getIncrementalValue(value);
+      const isMovingLeftOrDown =
+        event.key === 'ArrowLeft' || event.key === 'ArrowDown';
+      const isMovingRightOrUp =
+        event.key === 'ArrowRight' || event.key === 'ArrowUp';
+
+      if (isMovingLeftOrDown) {
+        event.preventDefault();
+        const newValue = value - incrementValue;
+        onChange(getNearestStepValue(newValue));
+      } else if (isMovingRightOrUp) {
+        event.preventDefault();
+        const newValue = value + incrementValue;
+        onChange(getNearestStepValue(newValue));
+      }
+    };
+
+    const sliderElement = sliderRef.current;
+    if (sliderElement) {
+      sliderElement.addEventListener('keydown', handleKeyDown);
+      return () => {
+        sliderElement.removeEventListener('keydown', handleKeyDown);
+      };
+    }
+  }, [value, onChange, defaultValue]);
 
   return (
     <Slider
@@ -62,15 +105,15 @@ const CustomSlider: FC<CustomSliderProps> = ({
       colorScheme='red'
       defaultValue={defaultValue}
       value={value}
-      // onChange={(value) => onChange(getNearestStepValue(value))}
-      onChange={onChange}
+      onChange={(value) => onChange(getNearestStepValue(value))}
       max={100}
+      min={0}
       data-cy={`${testId}-container`}
     >
       <SliderTrack bg='red.200'>
         <SliderFilledTrack bg='tomato' />
       </SliderTrack>
-      <SliderThumb bgColor='red.500' data-cy={testId} />
+      <SliderThumb ref={sliderRef} bgColor='red.500' data-cy={testId} />
     </Slider>
   );
 };
