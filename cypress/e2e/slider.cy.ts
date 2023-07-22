6c7f326f91ba4decc42611296095f9b54c8d3326
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-22 08:51:01 +0300

    Update tests

--- cypress/e2e/slider.cy.ts
+++ cypress/e2e/slider.cy.ts
@@ -6,9 +6,7 @@ const moveSlider = (args: {
   selector: string;
 }) => {
   for (let i = 0; i < args.times; i++) {
-    cy.get(`[data-cy=${args.selector}]`)
-      .focus()
-      .type(`{${args.direction}arrow}`);
+    cy.get(args.selector).focus().type(`{${args.direction}arrow}`);
   }
 };
 
@@ -16,70 +14,141 @@ const checkSliderValue = (args: {
   selector: string;
   value: string | number;
 }) => {
-  cy.get(`[data-cy=${args.selector}]`).should(
-    'have.attr',
-    'aria-valuenow',
-    args.value
-  );
+  cy.get(args.selector).should('have.attr', 'aria-valuenow', args.value);
 };
 
 describe('Levels tests', () => {
+  let levels: {
+    occupied: string;
+    powerSave: string;
+    minimum: string;
+  };
+
+  beforeEach(() => {
+    cy.fixture('levels').then((levelSelectors) => {
+      levels = levelSelectors;
+    });
+  });
+
   it('Occupied slider values change correctly', () => {
     cy.visit('/');
 
     checkSliderValue({
-      selector: 'occupied-slider',
+      selector: levels.occupied,
       value: defaultLevelValues.occupied,
     });
 
-    moveSlider({ times: 2, direction: 'right', selector: 'occupied-slider' });
-    checkSliderValue({ selector: 'occupied-slider', value: '90' });
+    moveSlider({ times: 2, direction: 'right', selector: levels.occupied });
+    checkSliderValue({ selector: levels.occupied, value: '90' });
 
-    moveSlider({ times: 10, direction: 'left', selector: 'occupied-slider' });
-    checkSliderValue({ selector: 'occupied-slider', value: '40' });
+    moveSlider({ times: 10, direction: 'left', selector: levels.occupied });
+    checkSliderValue({ selector: levels.occupied, value: '40' });
   });
 
   it('Power save slider values change correctly', () => {
     cy.visit('/');
 
     checkSliderValue({
-      selector: 'power-save-slider',
+      selector: levels.powerSave,
       value: defaultLevelValues.powerSave,
     });
 
-    moveSlider({ times: 5, direction: 'right', selector: 'power-save-slider' });
-    checkSliderValue({ selector: 'power-save-slider', value: '45' });
+    moveSlider({ times: 5, direction: 'right', selector: levels.powerSave });
+    checkSliderValue({ selector: levels.powerSave, value: '45' });
 
-    moveSlider({ times: 10, direction: 'left', selector: 'power-save-slider' });
-    checkSliderValue({ selector: 'power-save-slider', value: '0' });
+    moveSlider({ times: 10, direction: 'left', selector: levels.powerSave });
+    checkSliderValue({ selector: levels.powerSave, value: '0' });
   });
 
   it('Minimum slider values change correctly', () => {
     cy.visit('/');
 
     checkSliderValue({
-      selector: 'minimum-slider',
+      selector: levels.minimum,
       value: defaultLevelValues.minimum,
     });
 
-    moveSlider({ times: 10, direction: 'right', selector: 'minimum-slider' });
-    checkSliderValue({ selector: 'minimum-slider', value: '45' });
+    moveSlider({ times: 10, direction: 'right', selector: levels.minimum });
+    checkSliderValue({ selector: levels.minimum, value: '45' });
 
-    moveSlider({ times: 9, direction: 'left', selector: 'minimum-slider' });
-    checkSliderValue({ selector: 'minimum-slider', value: '1' });
+    moveSlider({ times: 9, direction: 'left', selector: levels.minimum });
+    checkSliderValue({ selector: levels.minimum, value: '1' });
   });
 
-  it('Checks if the correct values are logged', () => {
+  it('Checks if the correct values are emitted on apply', () => {
     cy.visit('/');
 
-    moveSlider({ times: 10, direction: 'right', selector: 'occupied-slider' });
+    moveSlider({ times: 10, direction: 'right', selector: levels.occupied });
     moveSlider({
       times: 10,
       direction: 'right',
-      selector: 'power-save-slider',
+      selector: levels.powerSave,
     });
-    moveSlider({ times: 10, direction: 'right', selector: 'minimum-slider' });
+    moveSlider({ times: 10, direction: 'right', selector: levels.minimum });
 
     cy.get('[data-cy=apply]').click();
+
+    cy.get('[data-cy=occupied-value]').should('have.text', '100');
+    cy.get('[data-cy=power-save-value]').should('have.text', '70');
+    cy.get('[data-cy=minimum-value]').should('have.text', '45');
+  });
+
+  it('Checks if the default values are emitted on cancel', () => {
+    cy.visit('/');
+
+    moveSlider({
+      times: 10,
+      direction: 'right',
+      selector: levels.occupied,
+    });
+    moveSlider({
+      times: 10,
+      direction: 'right',
+      selector: levels.powerSave,
+    });
+    moveSlider({ times: 10, direction: 'right', selector: levels.minimum });
+
+    cy.get('[data-cy=cancel]').click();
+
+    cy.get('[data-cy=occupied-value]').should(
+      'have.text',
+      defaultLevelValues.occupied
+    );
+    cy.get('[data-cy=power-save-value]').should(
+      'have.text',
+      defaultLevelValues.powerSave
+    );
+    cy.get('[data-cy=minimum-value]').should(
+      'have.text',
+      defaultLevelValues.minimum
+    );
+  });
+
+  it('Checks if occupied is always greater than power save and minimum', () => {
+    cy.visit('/');
+
+    moveSlider({ times: 15, direction: 'left', selector: levels.occupied });
+    checkSliderValue({ selector: levels.occupied, value: '5' });
+    checkSliderValue({ selector: levels.powerSave, value: '5' });
+
+    moveSlider({ times: 2, direction: 'left', selector: levels.occupied });
+    checkSliderValue({ selector: levels.occupied, value: '0' });
+    checkSliderValue({ selector: levels.powerSave, value: '0' });
+    checkSliderValue({ selector: levels.minimum, value: '0' });
+
+    moveSlider({
+      times: 10,
+      direction: 'right',
+      selector: levels.powerSave,
+    });
+    moveSlider({ times: 8, direction: 'right', selector: levels.minimum });
+    moveSlider({ times: 14, direction: 'left', selector: levels.occupied });
+
+    // moveSlider({
+    //   times: 10,
+    //   direction: 'right',
+    //   selector: levels.powerSave,
+    // });
+    // moveSlider({ times: 10, direction: 'right', selector: levels.minimum });
   });
 });
