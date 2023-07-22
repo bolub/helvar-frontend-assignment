9987297d360809ba9620c6476fcb093aabc3a2ec
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-22 10:16:16 +0300

    Add more test cases

--- cypress/e2e/slider.cy.ts
+++ cypress/e2e/slider.cy.ts
@@ -22,17 +22,23 @@ describe('Levels tests', () => {
     occupied: string;
     powerSave: string;
     minimum: string;
+    applyButton: string;
+    cancelButton: string;
+    occupiedValue: string;
+    powerSaveValue: string;
+    minimumValue: string;
+    closeModalButton: string;
   };
 
   beforeEach(() => {
     cy.fixture('levels').then((levelSelectors) => {
       levels = levelSelectors;
     });
+
+    cy.visit('/');
   });
 
   it('Occupied slider values change correctly', () => {
-    cy.visit('/');
-
     checkSliderValue({
       selector: levels.occupied,
       value: defaultLevelValues.occupied,
@@ -46,8 +52,6 @@ describe('Levels tests', () => {
   });
 
   it('Power save slider values change correctly', () => {
-    cy.visit('/');
-
     checkSliderValue({
       selector: levels.powerSave,
       value: defaultLevelValues.powerSave,
@@ -61,8 +65,6 @@ describe('Levels tests', () => {
   });
 
   it('Minimum slider values change correctly', () => {
-    cy.visit('/');
-
     checkSliderValue({
       selector: levels.minimum,
       value: defaultLevelValues.minimum,
@@ -76,8 +78,6 @@ describe('Levels tests', () => {
   });
 
   it('Checks if the correct values are emitted on apply', () => {
-    cy.visit('/');
-
     moveSlider({ times: 10, direction: 'right', selector: levels.occupied });
     moveSlider({
       times: 10,
@@ -86,16 +86,14 @@ describe('Levels tests', () => {
     });
     moveSlider({ times: 10, direction: 'right', selector: levels.minimum });
 
-    cy.get('[data-cy=apply]').click();
+    cy.get(levels.applyButton).click();
 
-    cy.get('[data-cy=occupied-value]').should('have.text', '100');
-    cy.get('[data-cy=power-save-value]').should('have.text', '70');
-    cy.get('[data-cy=minimum-value]').should('have.text', '45');
+    cy.get(levels.occupiedValue).should('have.text', '100');
+    cy.get(levels.powerSaveValue).should('have.text', '70');
+    cy.get(levels.minimumValue).should('have.text', '45');
   });
 
   it('Checks if the default values are emitted on cancel', () => {
-    cy.visit('/');
-
     moveSlider({
       times: 10,
       direction: 'right',
@@ -108,47 +106,126 @@ describe('Levels tests', () => {
     });
     moveSlider({ times: 10, direction: 'right', selector: levels.minimum });
 
-    cy.get('[data-cy=cancel]').click();
+    cy.get(levels.cancelButton).click();
 
-    cy.get('[data-cy=occupied-value]').should(
+    cy.get(levels.occupiedValue).should(
       'have.text',
       defaultLevelValues.occupied
     );
-    cy.get('[data-cy=power-save-value]').should(
+    cy.get(levels.powerSaveValue).should(
       'have.text',
       defaultLevelValues.powerSave
     );
-    cy.get('[data-cy=minimum-value]').should(
-      'have.text',
-      defaultLevelValues.minimum
-    );
+    cy.get(levels.minimumValue).should('have.text', defaultLevelValues.minimum);
   });
 
-  it('Checks if occupied is always greater than power save and minimum', () => {
-    cy.visit('/');
-
+  it('Checks if occupied is always greater than or equal to power save and minimum', () => {
+    // Move slider just enough to affect the power save slider and check if the values are equal
     moveSlider({ times: 15, direction: 'left', selector: levels.occupied });
-    checkSliderValue({ selector: levels.occupied, value: '5' });
-    checkSliderValue({ selector: levels.powerSave, value: '5' });
+    cy.get(levels.applyButton).click();
+    cy.get(levels.occupiedValue).then(($occupiedValue) => {
+      const occupied = parseInt($occupiedValue.text(), 10);
 
+      cy.get(levels.powerSaveValue).then(($powerSaveValue) => {
+        const powerSave = parseInt($powerSaveValue.text(), 10);
+
+        expect(occupied).to.equal(powerSave);
+      });
+    });
+    cy.get(levels.closeModalButton).click();
+
+    // Move slider to the far left end to check if all values are equal
     moveSlider({ times: 2, direction: 'left', selector: levels.occupied });
-    checkSliderValue({ selector: levels.occupied, value: '0' });
-    checkSliderValue({ selector: levels.powerSave, value: '0' });
-    checkSliderValue({ selector: levels.minimum, value: '0' });
+    cy.get(levels.applyButton).click();
+    cy.get(levels.occupiedValue).then(($occupiedValue) => {
+      const occupied = parseInt($occupiedValue.text(), 10);
 
+      cy.get(levels.powerSaveValue).then(($powerSaveValue) => {
+        const powerSave = parseInt($powerSaveValue.text(), 10);
+
+        cy.get(levels.minimumValue).then(($minimumValue) => {
+          const minimum = parseInt($minimumValue.text(), 10);
+          expect(occupied).to.equal(powerSave).to.equal(minimum);
+        });
+      });
+    });
+    cy.get(levels.closeModalButton).click();
+
+    // Move slider to the far right and confirm occupied is always greater than power save and minimum
     moveSlider({
       times: 10,
       direction: 'right',
-      selector: levels.powerSave,
+      selector: levels.occupied,
     });
-    moveSlider({ times: 8, direction: 'right', selector: levels.minimum });
-    moveSlider({ times: 14, direction: 'left', selector: levels.occupied });
 
-    // moveSlider({
-    //   times: 10,
-    //   direction: 'right',
-    //   selector: levels.powerSave,
-    // });
-    // moveSlider({ times: 10, direction: 'right', selector: levels.minimum });
+    cy.get(levels.applyButton).click();
+    cy.get(levels.occupiedValue).then(($occupiedValue) => {
+      const occupied = parseInt($occupiedValue.text(), 10);
+
+      cy.get(levels.powerSaveValue).then(($powerSaveValue) => {
+        const powerSave = parseInt($powerSaveValue.text(), 10);
+
+        cy.get(levels.minimumValue).then(($minimumValue) => {
+          const minimum = parseInt($minimumValue.text(), 10);
+          expect(occupied)
+            .to.be.greaterThan(powerSave)
+            .to.be.greaterThan(minimum);
+        });
+      });
+    });
+  });
+
+  it('Checks if power save is always greater than or equal to minimum but less than or equal to occupied', () => {
+    // Move slider just enough to affect the power save slider and check if the values are equal
+    moveSlider({ times: 5, direction: 'left', selector: levels.powerSave });
+    cy.get(levels.applyButton).click();
+    cy.get(levels.powerSaveValue).then(($powerSaveValue) => {
+      const powerSave = parseInt($powerSaveValue.text(), 10);
+
+      cy.get(levels.minimumValue).then(($minimumValue) => {
+        const minimum = parseInt($minimumValue.text(), 10);
+
+        expect(powerSave).to.equal(minimum);
+      });
+    });
+    cy.get(levels.closeModalButton).click();
+
+    // Move slider to the far right to check if power save is equal to occupied
+    moveSlider({ times: 18, direction: 'right', selector: levels.powerSave });
+    cy.get(levels.applyButton).click();
+    cy.get(levels.powerSaveValue).then(($powerSaveValue) => {
+      const powerSave = parseInt($powerSaveValue.text(), 10);
+
+      cy.get(levels.occupiedValue).then(($occupiedValue) => {
+        const occupied = parseInt($occupiedValue.text(), 10);
+
+        cy.get(levels.minimumValue).then(($minimumValue) => {
+          const minimum = parseInt($minimumValue.text(), 10);
+
+          expect(powerSave).to.equal(occupied);
+          expect(powerSave).to.be.greaterThan(minimum);
+        });
+      });
+    });
+    cy.get(levels.closeModalButton).click();
+  });
+
+  it('Checks if minimum is always less than or equal to power save and occupied', () => {
+    // Move slider to the far right and check if minimum is equal to power save and occupied
+    moveSlider({ times: 18, direction: 'right', selector: levels.minimum });
+    cy.get(levels.applyButton).click();
+    cy.get(levels.minimumValue).then(($minimumValue) => {
+      const minimum = parseInt($minimumValue.text(), 10);
+
+      cy.get(levels.powerSaveValue).then(($powerSaveValue) => {
+        const powerSave = parseInt($powerSaveValue.text(), 10);
+
+        cy.get(levels.occupiedValue).then(($occupiedValue) => {
+          const occupied = parseInt($occupiedValue.text(), 10);
+
+          expect(minimum).to.equal(powerSave).to.equal(occupied);
+        });
+      });
+    });
   });
 });
