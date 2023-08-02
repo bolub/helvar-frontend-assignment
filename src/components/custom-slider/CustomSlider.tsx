545282c5b2d23f91a838020dce6721a3bfb9e3f2
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-02 15:19:32 +0300

    refactor and cleanup

--- src/components/custom-slider/CustomSlider.tsx
+++ src/components/custom-slider/CustomSlider.tsx
@@ -7,7 +7,7 @@ import {
   SliderFilledTrack,
   SliderThumb,
 } from '@chakra-ui/react';
-import { getNearestStepValue } from './utils';
+import { getNearestStepValue, handleSliderKeyDown } from './utils';
 
 interface CustomSliderProps {
   defaultValue: number;
@@ -46,48 +46,15 @@ const CustomSliderComponent: FC<CustomSliderProps> = ({
 
   // Handling keyboard navigation
   useEffect(() => {
-    const getIncrementalValue = (value: number) => {
-      const isMovingUp = value > defaultValue;
-      const isMovingDown = value < defaultValue;
-
-      if (isMovingUp) {
-        if (value <= 1) return 1;
-        if (value === 2) return 3;
-      }
-
-      if (isMovingDown && value <= 1) {
-        return 1;
-      }
-
-      if (value === 5) return 4;
-
-      return 5;
-    };
-
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (value <= 0 || value > 100) return;
-
-      const incrementValue = getIncrementalValue(value);
-      const isMovingLeftOrDown =
-        event.key === 'ArrowLeft' || event.key === 'ArrowDown';
-      const isMovingRightOrUp =
-        event.key === 'ArrowRight' || event.key === 'ArrowUp';
-
-      if (isMovingLeftOrDown) {
-        event.preventDefault();
-        const newValue = value - incrementValue;
-        onChange(getNearestStepValue(newValue));
-      } else if (isMovingRightOrUp) {
-        event.preventDefault();
-        const newValue = value + incrementValue;
-
-        onChange(getNearestStepValue(newValue));
-      }
-    };
-
     const sliderElement = sliderRef.current;
+
     if (sliderElement) {
+      const handleKeyDown = (event: KeyboardEvent) => {
+        handleSliderKeyDown(event, value, defaultValue, onChange);
+      };
+
       sliderElement.addEventListener('keydown', handleKeyDown);
+
       return () => {
         sliderElement.removeEventListener('keydown', handleKeyDown);
       };
--- src/components/custom-slider/utils.ts
+++ src/components/custom-slider/utils.ts
@@ -1,58 +1,25 @@
-export const handleKeyBoardNavigation = ({
+export const getIncrementalValue = ({
   value,
   defaultValue,
-  sliderElement,
-  onChange,
 }: {
   value: number;
   defaultValue: number;
-  sliderElement: HTMLDivElement | null;
-  onChange: (value: number) => void;
 }) => {
-  const getIncrementalValue = (value: number) => {
-    const isMovingUp = value > defaultValue;
-    const isMovingDown = value < defaultValue;
+  const isMovingUp = value > defaultValue;
+  const isMovingDown = value < defaultValue;
 
-    if (isMovingUp) {
-      if (value <= 1) return 1;
-      if (value === 2) return 3;
-    }
-
-    if (isMovingDown && value <= 1) {
-      return 1;
-    }
-
-    if (value === 5) return 4;
-
-    return 5;
-  };
-
-  const handleKeyDown = (event: KeyboardEvent) => {
-    if (value <= 0 || value > 100) return;
-
-    const incrementValue = getIncrementalValue(value);
-    const isMovingLeftOrDown =
-      event.key === 'ArrowLeft' || event.key === 'ArrowDown';
-    const isMovingRightOrUp =
-      event.key === 'ArrowRight' || event.key === 'ArrowUp';
-
-    if (isMovingLeftOrDown) {
-      event.preventDefault();
-      const newValue = value - incrementValue;
-      onChange(getNearestStepValue(newValue));
-    } else if (isMovingRightOrUp) {
-      event.preventDefault();
-      const newValue = value + incrementValue;
-      onChange(getNearestStepValue(newValue));
-    }
-  };
-
-  if (sliderElement) {
-    sliderElement.addEventListener('keydown', handleKeyDown);
-    return () => {
-      sliderElement.removeEventListener('keydown', handleKeyDown);
-    };
+  if (isMovingUp) {
+    if (value <= 1) return 1;
+    if (value === 2) return 3;
   }
+
+  if (isMovingDown && value <= 1) {
+    return 1;
+  }
+
+  if (value === 5) return 4;
+
+  return 5;
 };
 
 export const getNearestStepValue = (value: number) => {
@@ -61,4 +28,29 @@ export const getNearestStepValue = (value: number) => {
   if (value >= 100) return 100;
 
   return Math.round(value / 5) * 5;
+};
+
+export const handleSliderKeyDown = (
+  event: KeyboardEvent,
+  value: number,
+  defaultValue: number,
+  onChange: (newValue: number) => void
+) => {
+  if (value <= 0 || value > 100) return;
+
+  const incrementValue = getIncrementalValue({
+    value,
+    defaultValue,
+  });
+
+  const isMovingLeftOrDown = ['ArrowLeft', 'ArrowDown'].includes(event.key);
+  const isMovingRightOrUp = ['ArrowRight', 'ArrowUp'].includes(event.key);
+
+  if (isMovingLeftOrDown || isMovingRightOrUp) {
+    const newValue = isMovingLeftOrDown
+      ? value - incrementValue
+      : value + incrementValue;
+    onChange(getNearestStepValue(newValue));
+    event.preventDefault();
+  }
 };
